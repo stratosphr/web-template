cac3b4e41a640aff4a33bcdf416678089d458bba
stratosphr <stratosphr@gmail.com>
2021-01-30 22:43:28 +0100

    Fixed warning

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,7 +19,7 @@ class UserFactory extends Factory {
      *
      * @return array
      */
-    public function definition() {
+    public function definition(): array {
         return [
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
